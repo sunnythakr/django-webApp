7336bee03a4263b3027845d4c0fdb08713bbc622
sunny <sunnyacademy0@gmail.com>
2021-03-16 16:08:20 +0530

    details Added

--- tubers/tubers/youtubers/views.py
+++ tubers/tubers/youtubers/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-
+from . models import Youtuber
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def youtubers(request):
-    pass
+    tubers =Youtuber.objects.order_by('-created_date')
+    data = {
+        'tubers':tubers
+    }
+
+    return render(request, 'youtubers/youtubers.html' , data)
 
 def youtubers_details(request, id):
-    pass
+    tuber =get_object_or_404(Youtuber, pk=id)
+    data={
+        'tuber':tuber,
+    }
+    return render(request, 'youtubers/youtuber_detail.html', data)
+    
 
 def search(request):
     pass
