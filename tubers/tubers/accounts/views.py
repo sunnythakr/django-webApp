f05966b961fcea358d534a0dd8cb8f35957b3535
sunny <sunnyacademy0@gmail.com>
2021-03-17 19:34:54 +0530

    Login feature added

--- tubers/tubers/accounts/views.py
+++ tubers/tubers/accounts/views.py
@@ -6,6 +6,21 @@ from django.contrib import messages, auth
 
 # Create your views here.
 def login(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password )
+
+        if user is not None:
+            auth.login(request, user)
+            messages.error(request,'you are logged in ')
+            return redirect('dashboard')
+        else:
+            messages.error(request,'invalid credentials')
+            return redirect('login')
+
+
     return render(request,'accounts/login.html')
 
 def register(request):
