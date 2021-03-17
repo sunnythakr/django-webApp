4f87ed43e2f6cae31ca8f3eca4be32b2053e3e7d
sunny <sunnyacademy0@gmail.com>
2021-03-17 18:22:57 +0530

    Register filed success

--- tubers/tubers/accounts/views.py
+++ tubers/tubers/accounts/views.py
@@ -17,11 +17,33 @@ def register(request):
         password = request.POST['password']
         corfirm_password = request.POST['confirm_password']
 
-        user  =User.objects.create_user(firstname=firstname, lastname=lastname, 
-        username=username,email=email, password=password)
-        user.save()
-        messages.success(request,'Account created Successfully')
-        return redirect('login')
+        if password == corfirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'Username Exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,'email already exist')
+                    return redirect('register')
+                else:
+                     user  =User.objects.create_user(first_name=firstname, last_name=lastname, 
+                     username=username,email=email, password=password)
+                     user.save()
+                     messages.success(request,'Account created Successfully')
+                     return redirect('login')
+
+        else:
+            messages.error(request,'password do not match')
+            return redirect('register')
+
+
+
+
+        # user  =User.objects.create_user(firstname=firstname, lastname=lastname, 
+        # username=username,email=email, password=password)
+        # user.save()
+        # messages.success(request,'Account created Successfully')
+        # return redirect('login')
 
     return render(request,'accounts/register.html')
 
