3ce86cda6f5ede623fc7ad256b645e82cac3d609
sunny <sunnyacademy0@gmail.com>
2021-03-18 16:06:17 +0530

    hiretubers app created

--- tubers/tubers/tubers/urls.py
+++ tubers/tubers/tubers/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('webpages.urls')),
     path('youtubers/', include('youtubers.urls')),
     path('accounts/', include('accounts.urls')),
-    path('socialaccounts/', include('allauth.urls'))
+    path('socialaccounts/', include('allauth.urls')),
+    path('hiretubers/', include('hiretubers.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
